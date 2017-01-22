bfbaf92ff1077e418386e1c57a95609def095418
joannepeach <jlee20@cca.edu>
2017-01-21 22:04:02 -0800

    Create puzzle-4.js

---
+++ puzzle-4.js
@@ -0,0 +1,18 @@
+down();
+down();
+down();
+down();
+right();
+var color=getColor();
+down();
+setColor(color);
+right();
+right();
+up();
+right();
+var color2=getColor();
+down();
+right();
+setColor(color2);
+right();
+right();
