f2ce78139b419f175d550eb63642b99710e95f97
joannepeach <jlee20@cca.edu>
2017-01-21 22:05:10 -0800

    Create puzzle-7.js

---
+++ puzzle-7.js
@@ -0,0 +1,32 @@
+while (getColor() == "") {
+  down();
+}
+while (getColor() == "red") { 
+  right();
+  right();
+}
+while (getColor() == "") {
+  up();
+}
+while (getColor() == "red") { 
+  right();
+  right();
+}
+while (getColor() == "") {
+  down();
+}
+while (getColor() == "red") { 
+  right();
+  right();
+}
+while (getColor() == "") {
+  up();
+}
+while (getColor() == "red") { 
+  right();
+  right();
+}
+while (getColor() == "") {
+  down();
+}
+right();
